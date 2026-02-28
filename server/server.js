f1779a4564c99fb6c8d4048f28ceb3fbfea808b0
Abhijeet Singh <abhi.aa1singh@gmail.com>
2026-02-28 09:09:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -73,3 +72,4 @@ app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 
 });
+
